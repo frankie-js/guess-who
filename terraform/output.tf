bb5a4cffcdc11006a55c09795ba52199a87cacbb
Frankie S <frankies.develop@gmail.com>
2024-01-20 18:58:34 +0000

    ✨ Output bucket name and url

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,11 @@
 ###########################################################
 # OUTPUTS
 ###########################################################
+
+output "website_bucket_name" {
+  value = local.website_bucket_name
+}
+
+output "website_bucket_url" {
+  value = "http://${local.website_bucket_name}.s3-website-us-east-1.amazonaws.com "
+}
